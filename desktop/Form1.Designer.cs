873114427c351efb13dba6499dfe72a53d6d64a0
Erdal Dalkiran <ed@erdaldalkiran.com>
2017-03-06 23:50:52 +0300

    #000 ed adds desktop version

---
+++ desktop/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(68, 25);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(185, 22);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 1;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Result:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(68, 81);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(192, 20);
+            this.txtResult.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ desktop/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace desktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            var parameter = txtInput.Text;
+
+            if (string.IsNullOrEmpty(parameter))
+            {
+                txtResult.Text = "Paramater has not a valid value!";
+                return;
+            }
+
+            var c = GetFirstNonRepeatingCharacterIfAny(parameter);
+
+            if (!c.HasValue)
+            {
+                txtResult.Text = "Unable to find any non repeating character!";
+                return;
+            }
+
+            txtResult.Text = c.Value.ToString();
+        }
+
+        private static char? GetFirstNonRepeatingCharacterIfAny(string parameter)
+        {
+            var mappings = FindCharactersAndCount(parameter);
+            var resultMaybe = mappings.Where(kvp => kvp.Value == 1).FirstOrDefault();
+            return resultMaybe.Value == 1 ? resultMaybe.Key : default(char?);
+        }
+
+        private static Dictionary<char, int> FindCharactersAndCount(string parameter)
+        {
+            var result = new Dictionary<char, int>();
+            foreach (var c in parameter)
+            {
+                if (result.ContainsKey(c))
+                {
+                    result[c] += result[c] + 1;
+                }
+                else
+                {
+                    result[c] = 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
